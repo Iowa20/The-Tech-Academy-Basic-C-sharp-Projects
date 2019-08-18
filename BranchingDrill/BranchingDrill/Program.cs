fcb311763b7ed5626dd850feb2a64745c3534787
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-18 18:01:56 -0400

    updated

---
+++ BranchingDrill/BranchingDrill/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            
+
+               if (packageWeight >= 51)
+               
+            {
+                Console.Write("Package too big to be shipped via Package Express.");
+            }
+            
+
+            Console.ReadLine();
+            Console.WriteLine("Please enter width:");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter height:");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter lenght:");
+            int packageLenght = Convert.ToInt32(Console.ReadLine());
+
+            int total = packageWidth + packageHeight + packageLenght;
+            if (total >= 51)
+            {
+                
+                Console.WriteLine("Package too big to be shipped via Pagkage Express");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+            int shippingTotal = total * packageWeight % 100;
+            
+            
+            
+            {
+               
+                Console.WriteLine("Your estimated total for shipping this pagkage is: " + shippingTotal);
+            }
+
+            Console.WriteLine("Thank you.");
+           
+            Console.ReadLine();
+        }
+    }
+}
