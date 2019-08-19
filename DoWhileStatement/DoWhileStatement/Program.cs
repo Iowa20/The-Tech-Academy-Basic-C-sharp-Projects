e4fe23b264f3356f2e880eb6db3381ffdc73072b
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-18 20:11:06 -0400

    update

---
+++ DoWhileStatement/DoWhileStatement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Countdown: 20 to 0");
+            int x = 20;
+            Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Countdown:" + x);
+                x--;
+            } while (x > 0);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ whileStatement/whileStatement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Count 1 to 20:");
+            
+            int x = 1;
+
+            while (x <= 20)
+            {
+                Console.WriteLine("Count: "+ x );
+                x++;
+                Console.ReadLine();
+            }
+
+        }
+
+    }
+}
