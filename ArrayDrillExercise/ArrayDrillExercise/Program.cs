ed8cbb88ccefe926a24d3fe00cdf364e365af964
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-25 21:05:53 -0400

    update

--- ArrayDrillExercise/ArrayDrillExercise/Program.cs
+++ ArrayDrillExercise/ArrayDrillExercise/Program.cs
@@ -10,33 +10,39 @@ namespace ArrayDrillExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Select an Index from 0 to 4.\nThen, the console will display the equivalent value of an index.");
-            string[] stringArray = { "uno", "dos", "tres", "cuatro", "cinco", };
 
-            int stringA = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("value is: " + stringArray[stringA]);
-
-            
-
-            Console.ReadLine();
-
-
-
-
-           
-
-            Console.WriteLine("Select an Index from 0 to 3. \nThen, the console will display the equivalent value of an index.");
-            int[] NumArray = { 20, 25, 55, 45 };
-
-            int input = int.Parse(Console.ReadLine());
-            Console.WriteLine("value is: " + NumArray[input]);
-
-            for (int inputB = 0; inputB > NumArray.Length; inputB++)
+            try
             {
-                Console.WriteLine("It is outside the index of the Array!!!");
+                Console.WriteLine("Select an Index from 0 to 4.\nThen, the console will display the equivalent value of an index.");
+                string[] stringArray = { "uno", "dos", "tres", "cuatro", "cinco", };
+
+                int stringA = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("value is: " + stringArray[stringA]);
+
+
+
+                Console.ReadLine();
+
+
+
+
+
+
+                Console.WriteLine("Select an Index from 0 to 3. \nThen, the console will display the equivalent value of an index.");
+                int[] NumArray = { 20, 25, 55, 45 };
+
+                int input = int.Parse(Console.ReadLine());
+                Console.WriteLine("value is: " + NumArray[input]);
+
+                for (int inputB = 0; inputB > NumArray.Length; inputB++)
+                {
+                    Console.WriteLine("It is outside the index of the Array!!!");
+                }
             }
-
-
+            catch (System.IndexOutOfRangeException )
+            {
+                Console.WriteLine("Index paramaeter is out of Range");
+            }
 
 
             Console.ReadLine();
