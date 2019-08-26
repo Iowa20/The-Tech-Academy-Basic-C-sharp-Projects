b6acd12b3b5b62dec7259ede2bf085df3e7f7018
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-25 20:23:37 -0400

    update

--- ArrayDrillExercise/ArrayDrillExercise/Program.cs
+++ ArrayDrillExercise/ArrayDrillExercise/Program.cs
@@ -13,17 +13,33 @@ namespace ArrayDrillExercise
             Console.WriteLine("Select an Index from 0 to 4.\nThen, the console will display the equivalent value of an index.");
             string[] stringArray = { "uno", "dos", "tres", "cuatro", "cinco", };
 
+            int stringA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("value is: " + stringArray[stringA]);
+
             
 
+            Console.ReadLine();
 
-            
-            int stringIndex = Convert.ToInt32(Console.ReadLine());
+
+
 
            
 
+            Console.WriteLine("Select an Index from 0 to 3. \nThen, the console will display the equivalent value of an index.");
+            int[] NumArray = { 20, 25, 55, 45 };
+
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine("value is: " + NumArray[input]);
+
+            for (int inputB = 0; inputB > NumArray.Length; inputB++)
+            {
+                Console.WriteLine("It is outside the index of the Array!!!");
+            }
 
 
-                    Console.ReadLine();
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ ListIntegerWithTryCatchBlock/ListIntegerWithTryCatchBlock/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListIntegerWithTryCatchBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here are the list in an array \nChoose a number in the list and divide that number from a number in the list.");
+            List<int> ListInt = new List<int>() { 2, 4, 6, 8, 9, 10 };
+            ListInt.ForEach(Console.WriteLine);
+            Console.WriteLine("Choose a number from the list:");
+
+            foreach (int numList in ListInt)
+            {
+                if (numList)
+
+
+            }
+
+       
+
+
+        }
+    }
+}
