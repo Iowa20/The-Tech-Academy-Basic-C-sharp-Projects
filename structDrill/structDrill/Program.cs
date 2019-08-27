e14b9ba11687515c0bad353f15a18cf7279a0d97
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-27 19:55:27 -0400

    update

---
+++ structDrill/structDrill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number Amount1 = new Number();
+             Amount1.Amount = 619;
+
+            Console.WriteLine("Value is: " + Amount1.Amount);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
