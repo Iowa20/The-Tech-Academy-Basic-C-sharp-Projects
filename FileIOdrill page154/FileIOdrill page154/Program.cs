3c3dab9671446e1dd7706aa9e18df3aa64d7e6a5
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-31 14:09:05 -0400

    update

---
+++ FileIOdrill page154/FileIOdrill page154/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileIOdrill_page154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Users\nin_i\logs\log.txt";
+
+            int Num;
+            Console.WriteLine("Please, type a number.");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number is: " + Num);
+            Console.ReadLine();
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+        }
+    }
+}
