cae12dd85d521046955874aef040ad4d0fa22e0e
Edmund D <52433772+Iowa20@users.noreply.github.com>
2019-08-31 20:26:26 -0400

    update

---
+++ dateTime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrillPage156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("Current Date & Time: " + now);
+
+            int Num1;
+            Console.WriteLine("Please enter any number:  ");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0:HH:mm:ss tt}", DateTime.Now);
+
+           
+
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
